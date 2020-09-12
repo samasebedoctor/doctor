4eda36e4ba3280669ac269800fb73fa37782d342
samasebedoctor <65858931+samasebedoctor@users.noreply.github.com>
2020-09-13 01:41:47 +0300

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,24 @@
     <div class="container">
       <Hero />
       <ProjectsGrid :projects="$page.projects.edges" />
+      <!-- GetButton.io widget -->
+<script type="text/javascript">
+    (function () {
+        var options = {
+            whatsapp: "+79620136777", // WhatsApp number
+            telegram: "sama_sebe_doctor", // Telegram bot username
+            call_to_action: "Уже интересно? Спрашивайте :)", // Call to action
+            button_color: "#A8CE50", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "whatsapp,telegram", // Order of buttons
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+</script>
+<!-- /GetButton.io widget -->
     </div>
     <LatestJournals :journals="$page.journals.edges" />
   </Layout>
@@ -46,21 +64,4 @@ export default {
     LatestJournals
   }
 }
-</script>
-
-<script>
-    (function () {
-        var options = {
-            whatsapp: "+79620136777", // WhatsApp number
-            telegram: "sama_sebe_doctor", // Telegram bot username
-            call_to_action: "Уже интересно? Спрашивайте :)", // Call to action
-            button_color: "#A8CE50", // Color of button
-            position: "right", // Position may be 'right' or 'left'
-            order: "whatsapp,telegram", // Order of buttons
-        };
-        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-    })();
 </script>
