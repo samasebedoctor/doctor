08721c1256189e25ef72ee9efa331d2114ccbc9d
samasebedoctor <65858931+samasebedoctor@users.noreply.github.com>
2020-09-13 01:12:12 +0300

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -17,25 +17,7 @@ export default {
 }
 </script>
 
- <!-- GetButton.io widget -->
-<script type="text/javascript">
-    (function () {
-        var options = {
-            whatsapp: "+79620136777", // WhatsApp number
-            telegram: "sama_sebe_doctor", // Telegram bot username
-            call_to_action: "Уже интересно? Спрашивайте :)", // Call to action
-            button_color: "#A8CE50", // Color of button
-            position: "right", // Position may be 'right' or 'left'
-            order: "whatsapp,telegram", // Order of buttons
-        };
-        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-    })();
-</script>
-<!-- /GetButton.io widget -->
-
+ 
 <style scoped>
 .footer {
     font-size: 0.8rem;
