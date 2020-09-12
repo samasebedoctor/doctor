7caf03d4d871e1848f97cae81ea80a4ffbfa205b
samasebedoctor <65858931+samasebedoctor@users.noreply.github.com>
2020-09-12 21:17:45 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,7 @@
             <div class="left">
                 <g-link :to="{ name: 'home' }" class="home-link">
                     <img 
-                        src="../../static/image.png"
+                        src="../../static/logo.png"
                         :alt="settings.site_name" 
                         class="logo"
                     />
@@ -22,7 +22,7 @@
 export default {
   data() {
     return {
-        logo: require("../../static/image.png"),
+        logo: require("../../static/logo.png"),
         settings: require("../../data/theme.json")
     }
   }
