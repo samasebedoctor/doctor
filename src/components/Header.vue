73d44b08cd159a4e4a7a8334fc48eed57258d301
samasebedoctor <65858931+samasebedoctor@users.noreply.github.com>
2020-09-13 02:01:57 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -35,12 +35,7 @@ export default {
     height: 6rem;
     z-index: 10;
 }
-.header.sticky {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-}
+
 .header > .container {
     display: flex;
     align-items: center;
