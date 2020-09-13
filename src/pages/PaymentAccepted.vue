d44121ae23e0a2164a82f9957e882c3fcd8e5f5c
samasebedoctor <65858931+samasebedoctor@users.noreply.github.com>
2020-09-13 19:45:39 +0300

    Update PaymentAccepted.vue

--- src/pages/PaymentAccepted.vue
+++ src/pages/PaymentAccepted.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="contact-header">
         <h1 class="contact-title">Добро пожаловать в мир красоты, молодости и здоровья!</h1>
-        <img src="assets/static/mirror.4c38f5d.fc8a943441b455a373182d4a12ccb02f.png">
+        <img src="/uploads/mirror.png">
         <h3 class="contact-title">Добро пожаловать в мир красоты, молодости и здоровья!</h3>
         <p>Я скоро свяжусь с Вами по указанному Вами телефону. Если у Вас возникли какие-либо вопросы, свяжитесь со мной через зеленую кнопку внизу справа!</p>
       </div>
