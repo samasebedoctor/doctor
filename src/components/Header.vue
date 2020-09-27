8eef493f2fb044caf004778c24404766d517012e
samasebedoctor <65858931+samasebedoctor@users.noreply.github.com>
2020-09-27 21:40:03 +0300

    Update Header.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,7 +12,7 @@
             </div>
             <nav class="nav right">
                 <g-link class="nav__link" to="/journal">Программа</g-link>
-                <g-link class="nav__link" to="/contact">Хочу к вам!</g-link>
+                <g-link class="nav__link" to="/contact">Хочу к вам в клуб!</g-link>
             </nav>
         </div>
     </header>
