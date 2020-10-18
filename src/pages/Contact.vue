2b9242528b4f4b5f830b4cc31510965e6ff91ef3
samasebedoctor <65858931+samasebedoctor@users.noreply.github.com>
2020-10-18 20:36:41 +0300

    Update Contact.vue

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -5,14 +5,14 @@
       <div class="contact-header">
         <h2 class="contact-title">Остался всего один шаг навстречу Вашей молодости!</h2>
         <p>Определите комфортный для Вас формат участия и жмите "Отправить".</p>
-        <p>После оплаты Вам нужно будет нажать на желтую кнопку "Вернуться на сайт", и откроется ссылка либо на скачивание документа, либо на закрытый Telegram канал.</p>
+        <p>После оплаты Вам нужно будет нажать на желтую кнопку "Вернуться на сайт", и откроется ссылка на закрытый Telegram канал.</p>
         <p>Если возникают проблемы, пишите мне в Instagram @sama_sebe_doctor, я помогу!</p>
       </div>
 
 <iframe src="https://promo-money.ru/quickpay/shop-widget?writer=seller&targets=%D0%A1%D1%85%D0%B5%D0%BC%D0%B0%20%D0%BE%D0%B7%D0%B4%D0%BE%D1%80%D0%BE%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D1%8F%20Sama%20sebe%20Doctor&targets-hint=&default-sum=390&button-text=12&payment-type-choice=on&fio=on&phone=on&hint=&successURL=https%3A%2F%2Ft.me%2Fjoinchat%2FAAAAAFbo-xKkJLnGGAZGmw&quickpay=shop&account=410016189735528" width="100%" height="244" frameborder="0" allowtransparency="true" scrolling="no"></iframe>
 
       <iframe src="https://promo-money.ru/quickpay/shop-widget?writer=seller&targets=%D0%9A%D0%BB%D1%83%D0%B1%20%D0%BE%D1%87%D0%B8%D1%89%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%B8%20%D0%BE%D0%BC%D0%BE%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BE%D1%80%D0%B3%D0%B0%D0%BD%D0%B8%D0%B7%D0%BC%D0%B0%203%20%D0%BC%D0%B5%D1%81%D1%8F%D1%86%D0%B0&targets-hint=&default-sum=4900&button-text=12&payment-type-choice=on&fio=on&phone=on&hint=&successURL=https%3A%2F%2Ft.me%2Fclub_sama_sebe_doctor&quickpay=shop&account=410016189735528" width="450" style="max-width:100%" height="270" frameborder="0" allowtransparency="true" scrolling="no"></iframe>      
-      <iframe src="https://promo-money.ru/quickpay/shop-widget?writer=seller&targets=%D0%9A%D0%BB%D1%83%D0%B1%20%D0%BE%D1%87%D0%B8%D1%89%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%B8%20%D0%BE%D0%BC%D0%BE%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BE%D1%80%D0%B3%D0%B0%D0%BD%D0%B8%D0%B7%D0%BC%D0%B0%201%20%D0%BC%D0%B5%D1%81%D1%8F%D1%86&targets-hint=&default-sum=1890&button-text=12&payment-type-choice=on&fio=on&phone=on&hint=&successURL=https%3A%2F%2Ft.me%2Fclub_sama_sebe_doctor&quickpay=shop&account=410016189735528" width="450" style="max-width:100%" height="270" frameborder="0" allowtransparency="true" scrolling="no"></iframe>      
+      <iframe src="https://promo-money.ru/quickpay/shop-widget?writer=seller&targets=%D0%9A%D0%BB%D1%83%D0%B1%20%D0%BE%D1%87%D0%B8%D1%89%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%B8%20%D0%BE%D0%BC%D0%BE%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F%20%D0%BE%D1%80%D0%B3%D0%B0%D0%BD%D0%B8%D0%B7%D0%BC%D0%B0%201%20%D0%BC%D0%B5%D1%81%D1%8F%D1%86&targets-hint=&default-sum=890&button-text=12&payment-type-choice=on&fio=on&phone=on&hint=&successURL=https%3A%2F%2Ft.me%2Fclub_sama_sebe_doctor&quickpay=shop&account=410016189735528" width="450" style="max-width:100%" height="270" frameborder="0" allowtransparency="true" scrolling="no"></iframe>      
       </div>
   </Layout>
 </template>
